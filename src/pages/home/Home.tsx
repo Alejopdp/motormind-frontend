c7748860c72406c3ce27bbdbaea16bb97c2a0790
Alejo Scotti <alejoscotti@gmail.com>
2025-02-21 16:06:50 -0300

    fix: build error fixed

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import { Form, InputGroup } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
