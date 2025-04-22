551a0a8331fc364bfd225e030e6e0b3178e96e76
mdaniotti <tinchodaniotti@gmail.com>
2025-04-22 20:45:07 -0300

    Add component ResouceLinkItems

---
+++ src/components/atoms/ResourceLinkItems/index.tsx
@@ -0,0 +1,21 @@
+export const ResourceLinkItems = ({
+  resources,
+}: {
+  resources: { label: string; url: string }[];
+}) => {
+  if (resources.length === 0) return null;
+  return (
+    <div className="space-y-2">
+      <h4 className="font-medium text-gray-800">Recursos:</h4>
+      <ul className="text-primary list-disc space-y-1 pl-6">
+        {resources.map((resource, idx) => (
+          <li key={idx}>
+            <a href={resource?.url} className="text-primary" target="_blank" rel="video_reference">
+              {resource?.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
